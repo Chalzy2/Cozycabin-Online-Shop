879cb104e5e0c691c78b8a065122c08aca145aed
Chalzy2 <98620897+Chalzy2@users.noreply.github.com>
2026-05-25 00:11:05 +0300

    Update products.js

--- products.js
+++ products.js
@@ -3,22 +3,29 @@ const products = {
 "BL1001":{
 name:"Ipcone Blender",
 price:"KES 2,500",
-image:"images/blender.jpg",
+image:"images/kitchen/blender.webp",
 category:"Kitchen"
 },
 
 "FIT2001":{
 name:"Mini Stepper",
 price:"KES 4,500",
-image:"images/stepper.jpg",
+image:"images/fitness/stepper.webp",
 category:"Fitness"
 },
+
 "FS3001":{
 name:"Luxury Loafers",
 price:"KES 3,000",
-image:"images/shoes.jpg",
+image:"images/fashion/loafers.webp",
 category:"Fashion"
+},
+
+"SL4001":{
+name:"Warm Home Slippers",
+price:"KES 1,500",
+image:"images/slippers/pink-slippers.webp",
+category:"Home"
 }
 
 };
-
