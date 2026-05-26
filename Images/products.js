d8d8abaf3cee6bd3a814e2f729c76af2ba1a751e
Chalzy2 <98620897+Chalzy2@users.noreply.github.com>
2026-05-26 13:15:13 +0300

    Create products.js

---
+++ Images/products.js
@@ -0,0 +1,40 @@
+const products = {
+
+shoes: [
+
+{
+title: "Classic Black Sneakers",
+
+price: 1999,
+
+sizes: [41,42,43,44],
+
+description:
+"Comfortable lightweight sneakers for daily wear.",
+
+images: [
+"images/fashion/shoes/black-shoe.webp",
+"images/fashion/shoes/gray-shoe.webp",
+"images/fashion/shoes/gold-shoe.webp"
+]
+},
+
+{
+title: "Ladies Fashion Sneakers",
+
+price: 1999,
+
+sizes: [41,42,43,44],
+
+description:
+"Stylish ladies sneakers with soft inner comfort.",
+
+images: [
+"images/fashion/shoes/ladies-white.webp",
+"images/fashion/shoes/ladies-black.webp"
+]
+}
+
+]
+
+};
