37f72e43ade04a7458fab111023bed68cd4553c4
Chalzy2 <98620897+Chalzy2@users.noreply.github.com>
2026-05-26 13:16:29 +0300

    Update products.js

--- Images/products.js
+++ Images/products.js
@@ -38,3 +38,73 @@ images: [
 ]
 
 };
+function showCategory(category){
+
+const container =
+document.getElementById("products-container");
+
+container.innerHTML = "";
+
+products[category].forEach((product,index)=>{
+
+container.innerHTML += `
+
+<div class="product-card">
+
+<img
+id="product-image-${index}"
+src="${product.images[0]}"
+>
+
+<h2>${product.title}</h2>
+
+<p>${product.description}</p>
+
+<h3 class="price">
+KES ${product.price}
+</h3>
+
+<p><b>Available Sizes:</b></p>
+
+<div class="sizes">
+
+${product.sizes.map(size => `
+<button>${size}</button>
+`).join("")}
+
+</div>
+
+<p><b>Colors:</b></p>
+
+<div class="color-options">
+
+${product.images.map((image,imgIndex)=>`
+
+<button
+class="color-btn"
+onclick="changeImage(${index}, '${image}')">
+</button>
+
+`).join("")}
+
+</div>
+
+<details>
+
+<summary>More Details</summary>
+
+<p>
+Premium quality fashion sneakers.
+Durable sole, breathable fabric,
+comfortable for long wear.
+</p>
+
+</details>
+
+</div>
+
+`;
+
+});
+
+}
